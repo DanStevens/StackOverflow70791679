10af81884c7c6c21bb1c3ab1eb857a0489e1ccb1
Dan Stevens <Daniel.STEVENS@oxinst.com>
2022-01-28 13:17:17 +0000

    Added code for second attempt at solving [StackOverflow question 70791679](https://stackoverflow.com/questions/70791679)

---
+++ ApiClient/MyApiClient.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyApi
+{
+    public interface IBase
+    {
+        string BaseProp1 { get; set; }
+        string BaseProp2 { get; set; }
+        string BaseProp3 { get; set; }
+    }
+    public interface IDerived : IBase
+    {
+        string DerivedPropA { get; set; }
+    }
+
+    public interface IMyApiClient
+    {
+        Task<ICollection<IBase>> GetAllAsync(CancellationToken cancellationToken = default);
+    }
+
+    public partial class Base : IBase {}
+
+    public partial class Derived : IDerived {}
+
+    public partial class MyApiClient : IMyApiClient
+    {
+        async Task<ICollection<IBase>> IMyApiClient.GetAllAsync(CancellationToken cancellationToken)
+        {
+            var resp = await GetAllAsync(cancellationToken).ConfigureAwait(false);
+            return resp.Select(o => (IBase) o.Properties).ToList();
+        }
+    }
+}
---
+++ ApiClientTests/Tests.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using MyApi;
+using NUnit.Framework;
+
+namespace ApiClientTests
+{
+    public class Tests
+    {
+        private readonly IBase[] _allObjects = {
+            new Base {BaseProp1 = "Alpha", BaseProp2 = "Bravo", BaseProp3 = "Charlie"},
+            new Derived {BaseProp1 = "Delta", BaseProp2 = "Echo", BaseProp3 = "Foxtrot", DerivedPropA = "Golf"}
+        };
+
+        [Test]
+        public void ShouldBeAbleToAccessPropertiesOnBaseAndDerivedTypes()
+        {
+            IBase baseObject = _allObjects[0];
+            Assert.That(baseObject, Is.TypeOf<Base>());
+            Assert.That(baseObject.BaseProp1, Is.EqualTo("Alpha"));
+
+            IDerived derivedObject = (IDerived)_allObjects[1];
+            Assert.That(derivedObject, Is.TypeOf<Derived>());
+            Assert.That(derivedObject.DerivedPropA, Is.EqualTo("Golf"));
+        }
+
+        [Test]
+        public void ShouldBeAbleToDiscriminateDerivativeTypesUsingTypeCasting()
+        {
+            IDerived[] derivatives = _allObjects.OfType<IDerived>().ToArray();
+            Assert.That(derivatives.Length, Is.EqualTo(1));
+            Assert.That(derivatives[0], Is.SameAs(_allObjects[1]));
+        }
+
+
+        [Ignore("Example usage only - API host doesn't exist")]
+        [Test]
+        public async Task TestGetAllOperation()
+        {
+            using var httpClient = new HttpClient();
+            IMyApiClient apiClient = new MyApiClient("https://example.io/", httpClient);
+            var resp = await apiClient.GetAllAsync();
+            Assert.That(resp, Is.TypeOf<ICollection<IBase>>());
+
+            IBase[] allObjects = resp.ToArray();
+            Assert.That(allObjects.Length, Is.EqualTo(2));
+            Assert.That(allObjects[0].BaseProp1, Is.EqualTo("Alpha"));
+            Assert.That(((IDerived)allObjects[1]).DerivedPropA, Is.EqualTo("Golf"));
+        }
+    }
+}
